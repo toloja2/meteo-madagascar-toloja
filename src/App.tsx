543582b398b182fd4b2e05589c0920b2c34bc35f
Tolojanahary Ranarijaonina <tolojaranarijaonina@gmail.com>
2026-02-19 13:43:23 +0300

    Add select country, update url API and somes styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import defaultMeteo from './assets/default.webp';
 import drizzleMeteo from './assets/drizzle.webp';
 import rainMeteo from './assets/rain.webp';
 import thunderstormMeteo from './assets/thunderstorm.webp';
-import drapeauMada from './assets/drapeauMada.webp';
+// import drapeauMada from './assets/drapeauMada.webp';
 
 function App() {
 
@@ -17,22 +17,42 @@ function App() {
   const [weather, setWeather] = useState<WeatherReponse | null>(null);
   const [loading, setLoading] = useState(false);
   const [erreur, setErreur] = useState("");
+  const [codePays, setCodePays] = useState("MG");
+  // const [nomPays, setNomPays] = useState<string>("");
+
+  const dataPays = [
+    { nom: "Madagascar", code: "MG" },
+    { nom: "France", code: "FR" },
+    { nom: "United States", code: "US" },
+    { nom: "United Kingdom", code: "GB" },
+    { nom: "Germany", code: "DE" },
+    { nom: "Italy", code: "IT" },
+    { nom: "Spain", code: "ES" },
+    { nom: "Canada", code: "CA" },
+    { nom: "Chine", code: "CN" },
+    { nom: "Japan", code: "JP" },
+    { nom: "Indian", code: "In" },
+    { nom: "South Africa", code: "ZA" },
+    { nom: "Australia", code: "AU" },
+
+  ]
+
   const handleSearch = async () => {
-    if (!city) {
+    if (!city || !codePays) {
       return;
     }
 
     try {
       setLoading(true);
       setErreur("");
-      const data = await WeatherApi(city);
+      const data = await WeatherApi(city, codePays);
       setWeather(data);
-      console.log(data);
+      console.log(weather);
 
     }
 
     catch {
-      setErreur("Ville introuvable");
+      setErreur("City not found");
       setWeather(null);
     }
 
@@ -77,29 +97,38 @@ function App() {
 
 
   return (
-    <div className={`min-h-screen flex justify-center items-center bg-linear-to-br ${background} transition-all duration-1000 ease-in-out `}>
-      <div className="animation-fadeIn bg-white/50 border border-white/30 backdrop-blur-3xl p-8 rounded-3xl shadow-2xl w-[95%] max-w-lg text-white transition duration-500 hover:scale-105 hover:shadow-yellow-500/50 hover:ring-1 hover:ring-orange-600">
+    <div className={`min-h-screen  flex justify-center items-start bg-linear-to-br ${background} transition-all duration-1000 ease-in-out `}>
+      <div className="animation-fadeIn mt-10 bg-white/50 border border-white/30 backdrop-blur-3xl p-8 rounded-3xl shadow-2xl w-[95%] max-w-lg text-white transition duration-500 hover:scale-105 hover:shadow-yellow-500/50 hover:ring-1 hover:ring-orange-600">
 
-        <h1 className="mx-16 flex gap-6 justify-items-center items-center  text-center font-extrabold mb-6 text-2xl bg-linear-to-r from-yellow-500 to-white bg-clip-text text-transparent">
-          Météo Madagascar <img src={drapeauMada} alt="drapeau madagascar" className='w-7 h-7' />
+        <h1 className="w-full text-center font-extrabold mb-6 text-2xl bg-linear-to-r from-yellow-600 to-white bg-clip-text text-transparent">
+          Weather {!weather?.name ? "App":`in ${weather?.name}` }
+          {/* Météo <img src={drapeauMada} alt="drapeau madagascar" className='w-7 h-7' /> */}
         </h1>
+        <select required value={codePays} onChange={(e) =>setCodePays(e.target.value)} className='text-sm font-bold tracking-widest w-full mb-5 outline-0 ring-1 ring-amber-400 rounded p-2 text-gray-800 bg-white/90 backdrop-blur-xl '>
+          {/* <option value="" disabled className='text-xl '>Select country...</option> */}
+          {
+            dataPays.map((c) => (
+              <option className='' key={c.code} value={c.code}>{c.nom}</option>
+            ))
+          }
+        </select>
 
-        <input type="text" placeholder='Entrer une ville ...' className='outline-none text-black w-full bg-white/50 rounded-xs text-center p-1 focus:ring-1 focus:ring-yellow-200 transition' value={city} onChange={(e) => setCity(e.target.value)} />
-        <button onClick={handleSearch} className='w-full mt-4 bg-white text-blue-600 py-2 font-semibold rounded-lg hover:bg-blue-100 hover:ring-1 hover:ring-red-100 transition ease-in-out duration-300'>
-          {loading ? "Chargement..." : "Rechercher"}
+        <input required type="text" placeholder='Enter a city ...' className='outline-none text-black w-full bg-white/50 rounded-xs text-center p-1 focus:ring-1 focus:ring-yellow-200 transition' value={city} onChange={(e) => setCity(e.target.value)} />
+        <button type='submit' onClick={handleSearch} className={`${!city || !codePays ? "bg-white/20 text-blue-300 hover:ring-0" :""} disabled: w-full mt-4 bg-white text-blue-600 py-2 font-semibold rounded-lg hover:bg-blue-100 hover:ring-1 hover:ring-red-100 transition ease-in-out duration-300`}>
+          {loading ? "Loading..." : "Search"}
         </button>
 
         {erreur && (
-          <p className='text-center my-4 text-red-500 font-bold'>{erreur}</p>
+          <p className='text-center my-4 text-red-500 bg-red-200 rounded-xl p-2  font-bold'>{erreur}</p>
         )}
 
         {weather && (
           <>
-            <p className='capitalize mt-8 text-center text-xl font-bold text-black '>{weather.weather[0].description}</p>
-            <img src={urlImages(weather.weather[0].main.toLowerCase())} alt='image icon' className='my-10 text-center mx-auto' />
+            <p className='capitalize mt-8 text-center text-xl font-bold text-black '>In {weather?.name} , {weather.weather[0].description}</p>
+            <img src={urlImages(weather.weather[0].main.toLowerCase())} alt='image icon' className='my-2 text-center mx-auto' />
             <div className="grid grid-cols-2 mt-5 gap-4 text-center">
               <div className="bg-white/20 p-4 rounded-xl animation-fadeIn">
-                <p className='text-sm'>Ville</p>
+                <p className='text-sm'>City</p>
                 <p className='font-bold text-2xl'>{weather.name}</p>
               </div>
               <div className="bg-white/20 p-4 rounded-xl animation-fadeIn">
@@ -107,11 +136,11 @@ function App() {
                 <p className='font-bold text-2xl'>{weather.main.temp} °C</p>
               </div>
               <div className="bg-white/20 p-4 rounded-xl animation-fadeIn">
-                <p className='text-sm'>Humidité</p>
+                <p className='text-sm'>Humidity</p>
                 <p className='font-bold text-2xl'>{weather.main.humidity}%</p>
               </div>
               <div className="bg-white/20 p-4 rounded-xl animation-fadeIn">
-                <p className='text-sm'>Vent</p>
+                <p className='text-sm'>Wind</p>
                 <p className='font-bold text-2xl'>{weather.wind.speed} m/s</p>
               </div>
             </div>
--- src/services/WeatherApi.ts
+++ src/services/WeatherApi.ts
@@ -3,11 +3,11 @@ import {type WeatherReponse } from "../types/Weather";
 const API_KEY = "931b7c5550183d5e2a9392e196b525b6";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-const WeatherApi = async(city:string):Promise<WeatherReponse>=>{
-    const response = await fetch(`${BASE_URL}?q=${city}&country=MG&lang=fr&units=metric&appid=${API_KEY}`);
+const WeatherApi = async(city:string,country:string):Promise<WeatherReponse>=>{
+    const response = await fetch(`${BASE_URL}?q=${city}&country=${country}&lang=fr&units=metric&appid=${API_KEY}`);
 
     if(!response.ok){
-        throw new Error("Ville introuvable")
+        throw new Error("Please verify your network")
     }
 
     return response.json();
